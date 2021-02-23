f9b4a34533ba8f22809f30107fca5889fb104f43
Monaldcry7788 <63928518+Monaldcry7788@users.noreply.github.com>
2021-02-23 21:30:28 +0100

    1.0.0 update

---
+++ Infiltrated2.0/Commands/Kill.cs
@@ -0,0 +1,61 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using System;
+namespace Infiltrated.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    class Kill : ICommand
+    {
+        public string Command { get; } = "Infiltrated_kill";
+
+        public string[] Aliases { get; } = new[] { "IKill", "infilt_kill" };
+
+        public string Description { get; } = "Kill an Infiltrated";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (sender.CheckPermission("infiltrated.kill"))
+            {
+                if (arguments.Count == 1)
+                {
+                    Player target = Player.Get(arguments.At(0));
+                    if (target != null)
+                    {
+                        if (Infiltrated.Instance.infiltrates.Contains(target))
+                        {
+                            response = $"Player {target.Nickname} has been killed";
+                            Events.Kill(target);
+                            return true;
+                        }
+                        else
+                        {
+                            response = $"You can't kill {target.Nickname} Because he is not an infiltrated";
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        response = "Player not found";
+                        return false;
+                    }
+                }
+
+
+                else if (arguments.Count != 1)
+                {
+                    response = $"Usage: {Command}/IKill <player name / id>";
+                    return false;
+                }
+            }
+            else
+            {
+                response = "<color=red>You can't do this command!</color>";
+                return false;
+            }
+
+            response = null;
+            return false;
+        }
+    }
+}
---
+++ Infiltrated2.0/Config.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace Infiltrated
+{
+    public class Config : IConfig
+    {
+        [Description("Enable or Disable the plugin")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("The items Infiltrated spawn with.")]
+        public List<ItemType> InfiltratedItems { get; private set; } = new List<ItemType>() { ItemType.KeycardScientistMajor, ItemType.KeycardSeniorGuard, ItemType.Medkit };
+
+        [Description("The broadcast to all ClassD")]
+        public Exiled.API.Features.Broadcast ClassDBroadcast { get; set; } = new Exiled.API.Features.Broadcast("<size=30><color=aqua>In {seconds} seconds a classD will be chosen to become an</color> <color=red>Infiltrated ClassD</color></size>", 10);
+
+        [Description("How soon to spawn Infiltrated ClassD")]
+        public float ClassDSpawnTime { get; set; } = 30.0f;
+
+        [Description("The broadcast to Infiltrated ClassD")]
+        public Exiled.API.Features.Broadcast InfiltratedBroadcast { get; private set; } = new Exiled.API.Features.Broadcast("<size=30><color=aqua>You have become a</color> <color=red>classD Infiltrated</color>\n <color=aqua>Help the other ClassDs escape the facility!</color></size>", 10);
+
+        [Description("Healt amount of Infiltrated ClassD")]
+        public int HealthAmount { get; private set; } = 150;
+
+        [Description("The broadcast when Infiltrated death")]
+        public Exiled.API.Features.Broadcast InfiltratedBroadcastDeath { get; private set; } = new Exiled.API.Features.Broadcast("<color=aqua>The infiltrated <color=red>{player}</color> has been!</color>", 10);
+
+    }
+}
