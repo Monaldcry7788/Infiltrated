80273c110f6b3a1aea4904463ff0edf8ba2795f5
Monaldcry7788 <63928518+Monaldcry7788@users.noreply.github.com>
2021-02-26 22:31:04 +0100

    2.0.0

---
+++ Infiltrated2.0/Commands/List.cs
@@ -0,0 +1,70 @@
+﻿using CommandSystem;
+using Exiled.Permissions.Extensions;
+using NorthwoodLib.Pools;
+using System;
+using System.Text;
+using Exiled.API.Features;
+
+namespace Infiltrated.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class List : ICommand
+    {
+        public string Command { get; } = "Infiltrated_list";
+
+        public string[] Aliases { get; } = new[] { "IList", "infiltr_list" };
+
+        public string Description { get; } = "Show all Infiltrated alives";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            /*if (sender.CheckPermission("infiltrated.list"))
+            {
+                if (arguments.Count == 0)
+                {
+                    StringBuilder text = StringBuilderPool.Shared.Rent();
+                    text.AppendLine($"[INFILTRATED ALIVE ({Infiltrated.Singleton.TrackedPlayers.Count})]");
+
+                    foreach (var infiltrated in Infiltrated.Singleton.TrackedPlayers)
+                    {
+                        text.AppendLine($"[Player: {infiltrated.Nickname} ({infiltrated.UserId})]")
+                            .AppendLine($"[Player Health: {infiltrated.Health}HP]")
+                            .AppendLine($"[Player Role: {infiltrated.Role}]");
+                    }
+                    response = StringBuilderPool.Shared.ToStringReturn(text);
+                    return true;
+                }
+                    response = "Usage: Infiltrated_list/IList";
+                    return false;
+            }
+            response = "<color=red>You can't do this command!</color>";
+            return false;
+            */
+
+            if (!sender.CheckPermission("infiltrated.list"))
+            {
+                response = "<color=red>You can't do this command!</color>";
+                return false;
+            }
+
+            if (arguments.Count != 0)
+            {
+                response = "Usage: Infiltrated_list/IList";
+                return false;
+            }
+
+            StringBuilder text = StringBuilderPool.Shared.Rent();
+            text.AppendLine($"[INFILTRATED ALIVE ({Infiltrated.Singleton.TrackedPlayers.Count})]");
+
+            foreach (var infiltrated in Infiltrated.Singleton.TrackedPlayers)
+            {
+                text.AppendLine($"[Player: {infiltrated.Nickname} ({infiltrated.UserId})]")
+                    .AppendLine($"[Player Health: {infiltrated.Health}HP]")
+                    .AppendLine($"[Player Role: {infiltrated.Role}]");
+            }
+            response = StringBuilderPool.Shared.ToStringReturn(text);
+            return true;
+
+        }
+    }
+}
---
+++ Infiltrated2.0/Events.cs
@@ -0,0 +1,41 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System.Collections.Generic;
+using System.Linq;
+namespace Infiltrated
+{
+    public class Events
+    {
+        public System.Random Rand = new System.Random();
+        private readonly Infiltrated plugin;
+        private readonly Logic logic;
+        public Events(Infiltrated plugin) => this.plugin = plugin;
+        public Events(Logic logic) => this.logic = logic;
+
+        public void OnRoundStart()
+        {
+            plugin.TrackedPlayers.Clear();
+            Timing.CallDelayed(2.0f, () =>
+            {
+                foreach (Player player in Player.List)
+                {
+                    if (player.Role == RoleType.ClassD)
+                    {
+                        player.Broadcast(Infiltrated.Singleton.Config.ClassDBroadcast.Duration, Infiltrated.Singleton.Config.ClassDBroadcast.Content.Replace("{seconds}", Infiltrated.Singleton.Config.ClassDSpawnTime.ToString()));
+                    }
+                }
+            });
+            Timing.CallDelayed(plugin.Config.ClassDSpawnTime, () => logic.ChooseClassD());
+        }
+        public void OnDied(DiedEventArgs ev)
+        {
+            if (plugin.TrackedPlayers.Contains(ev.Target) && Infiltrated.Singleton.Config.InfiltratedBroadcastDeath.Show)
+            {
+                Map.Broadcast(Infiltrated.Singleton.Config.InfiltratedBroadcastDeath.Duration, Infiltrated.Singleton.Config.InfiltratedBroadcastDeath.Content.Replace("{player}", ev.Target.Nickname));
+                plugin.TrackedPlayers.Remove(ev.Target);
+            }
+        }
+    }
+}
